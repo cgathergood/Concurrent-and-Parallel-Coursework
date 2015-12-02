1962ac2aab687991e9a00e6b87ab653e01bafe17
Calum Gathergood <calumgathergood@gmail.com>
2015-12-02 11:47:42 +0000

    Re-added the cudaInfo method

--- Coursework2/Coursework2/Coursework2/CUDA.cu
+++ Coursework2/Coursework2/Coursework2/CUDA.cu
@@ -1,10 +1,30 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
+#include <iostream>
+
+using namespace std;
+
+void cudaInfo()
+{
+	int device;
+	cudaGetDevice(&device);
+
+	cudaDeviceProp properties;
+	cudaGetDeviceProperties(&properties, device);
+
+	cout << "Name: " << properties.name << endl;
+	cout << "CUDA Capability: " << properties.major << "." << properties.minor << endl;
+	cout << "Cores: " << properties.multiProcessorCount << endl;
+	cout << "Memory: " << properties.totalGlobalMem / (1024 * 1024) << "MB" << endl;
+	cout << "Clock Freq: " << properties.clockRate / 1000 << "MHz" << endl;
+}
 
 int main()
 {
 	// Initialise CUDA - select device
 	cudaSetDevice(0);
+
+	cudaInfo();
 
 	return 0;
 }
