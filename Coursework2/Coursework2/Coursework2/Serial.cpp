6c774ca299dceac6aa0b3868e54d2674dfff6a56
Calum Gathergood <calumgathergood@gmail.com>
2015-12-03 15:13:05 +0000

    Refactored serial version with working visualisation.

--- Coursework2/Coursework2/Coursework2/Serial.cpp
+++ Coursework2/Coursework2/Coursework2/Serial.cpp
@@ -12,14 +12,14 @@ using namespace std::chrono;
 
 // Output file
 ofstream dataFileOutput("data.csv", ofstream::out);
-
 // Number of bodies
-const int N = 4;
+const int N = 100;
+// Number of iterations
+const int iterations = 100;
 
 // N-Body constants
-const double G = 6.673e-11; // Gravitational Constant
-const double solarmass = 1.98892e30;
-const double radiusOfUniverse = 1e18;
+const float G = 6.673e-11; // Gravitational Constant
+const float radiusOfUniverse = 1000.0f;
 
 // create and initiliase a new body
 struct Body
@@ -41,149 +41,87 @@ string PrintBody(Body body)
 	return ss.str();
 }
 
-// update velocity and position using timestamp dt
-Body Update(Body body, double dt)
-{
-	body.vx += dt * body.fx / body.mass;
-	body.vy += dt * body.fy / body.mass;
-	body.rx += dt * body.vx;
-	body.ry += dt * body.vy;
-	return body;
-}
-
-// returns distace between two bodies
-double distanceTo(Body a, Body b)
-{
-	double dx = a.rx - b.rx;
-	double dy = a.ry - b.ry;
-	return sqrt(dx * dx + dy * dy);
-}
-
-// set force to 0 for the next iteration
-Body ResetForce(Body body)
-{
-	body.fx = 0.0;
-	body.fy = 0.0;
-	return body;
-}
-
-// compute the net force acting between the body a and b, and add to the net force acting on a
-Body AddForce(Body a, Body b)
-{
-	double EPS = 3E4; // softening parameter (just to avoid infinities)
-	double dx = b.rx - a.rx;
-	double dy = b.ry - a.ry;
-	double dist = sqrt(dx * dx + dy * dy);
-	double F = (G * a.mass * b.mass) / (dist * dist + EPS * EPS);
-	a.fx += F * dx / dist;
-	a.fy += F * dy / dist;
-	return a;
-}
-
 // Random number generator
 float randomGenerator(float min, float max)
 {
 	return ((float(rand()) / float(RAND_MAX)) * (max - min)) + min;
 }
 
-// bodies are initliased in circular orbits around the central mass. This is the physics to do that
-double circlev(double rx, double ry)
-{
-	double r2 = sqrt(rx * rx + ry * ry);
-	double numerator = (G) * 1e6 * solarmass;
-	return sqrt(numerator / r2);
-}
-
-// template for the signum function contained within Java's Math library
-template <typename T>
-int signum(T val)
-{
-	return (T(0) < val) - (val < T(0));
-}
-
 // Initialise N bodies with random positions and circular velocities
-void startTheBodies()
+void startTheBodies(Body* bodies)
 {
-	for (int i = 0; i < N; ++i)
+	for (auto i = 0; i < N; ++i)
 	{
-		double px = 1e18 * exp(-1.8) * (.5 - randomGenerator(0.0, 1.0));
-		double py = 1e18 * exp(-1.8) * (.5 - randomGenerator(0.0, 1.0));
-		double magv = circlev(px, py);
-
-		double absangle = atan(abs(py / px));
-		double thetav = M_PI / 2 - absangle;
-		double vx = -1 * signum(py) * cos(thetav) * magv;
-		double vy = signum(px) * sin(thetav) * magv;
-
-		// Orientate a random 2D cirular orbit
-		if (randomGenerator(0.0, 1.0) <= 0.5)
-		{
-			vx = -vx;
-			vy = -vy;
-		}
-
-		// Calculate mass
-		double mass = solarmass * randomGenerator(0.0, 1.0) * 1e20;
-		// Assign variables to a body struct
+		auto px = randomGenerator(0, radiusOfUniverse);
+		auto py = randomGenerator(0, radiusOfUniverse);
+		auto m = randomGenerator(0, 10e4);
 		bodies[i].rx = px;
 		bodies[i].ry = py;
-		bodies[i].vx = vx;
-		bodies[i].vy = vy;
-		bodies[i].mass = mass;
+		bodies[i].vx = 0.0f;
+		bodies[i].vy = 0.0f;
+		bodies[i].mass = m;
 	}
-};
+}
 
-void addForces()
+void updateForces(Body* bodies, float dt)
 {
-	for (int i = 0; i < N; ++i)
+	for (auto i = 0; i < N; ++i)
 	{
-		bodies[i] = ResetForce(bodies[i]);
-		for (int j = 0; j < N; ++j)
+		// Reset Forces
+		auto fx = 0.0f;
+		auto fy = 0.0f;
+
+		for (auto j = 0; j < N; ++j)
 		{
 			if (i != j)
 			{
-				bodies[i] = AddForce(bodies[i], bodies[j]);
+				// Calcululate new forces
+				float dx = bodies[j].rx - bodies[i].rx;
+				float dy = bodies[j].ry - bodies[i].ry;
+				auto distsqr = dx * dx + dy * dy + 1e-9f; //softening
+				auto distSixth = distsqr * distsqr;
+				auto invDist = 1.0f / sqrtf(distSixth);
+				float F = bodies[j].mass * invDist;
+				fx += F * dx;
+				fy += F * dy;
 			}
 		}
+		// Calculate new velcocity (based on new force)
+		bodies[i].vx += dt * fx;
+		bodies[i].vy += dt * fy;
 	}
-
-	// Loop again, update bodies with timestamp
-	for (int i = 0; i < N; ++i)
+	// Calculate new position (based on new velocity)
+	for (auto i = 0; i < N; ++i)
 	{
-		bodies[i] = Update(bodies[i], 1e11);
+		bodies[i].rx += bodies[i].vx * dt;
+		bodies[i].ry += bodies[i].vy * dt;
 	}
 }
 
 void drawImage(Body bodies[N], int name)
 {
 	FreeImage_Initialise();
-	FIBITMAP* bitmap = FreeImage_Allocate(100*2, 100*2, 24);
+	auto bitmap = FreeImage_Allocate(radiusOfUniverse, radiusOfUniverse, 24);
 	RGBQUAD color;
 
-	for (int i = 0; i < N; i++)
+	for (auto i = 0; i < N; i++)
 	{
-		if (i == 1)
-		{
-			cout << "X:" << round(bodies[i].rx / 1e30 + N) << ", Y:" << round(bodies[i].ry * 10 / 1e30 + N) << endl;
-		}
-
 		color.rgbGreen = 255;
 		color.rgbBlue = 255;
 		color.rgbRed = 255;
-		FreeImage_SetPixelColor(bitmap, round(bodies[i].rx / 1e30+100), round(bodies[i].ry / 1e30+100), &color);
+		FreeImage_SetPixelColor(bitmap, bodies[i].rx, bodies[i].ry, &color);
 	}
 
 	// Creates a numbered file name
 	stringstream fileName;
 	fileName << name << "test.png";
-	cout << fileName.str() << endl;
 	char file[1024];
 	strcpy(file, fileName.str().c_str());
 
 	// Save the file
 	if (FreeImage_Save(FIF_PNG, bitmap, file, 0))
 	{
-		cout << "Image successfully saved!" << endl;
+		cout << "Image saved - " << fileName.str() << endl;
 	}
 
 	FreeImage_DeInitialise();
@@ -191,24 +129,24 @@ void drawImage(Body bodies[N], int name)
 
 int main()
 {
-	auto start = system_clock::now();
-	startTheBodies();
-
-	for (int i = 0; i < 400; ++i)
+	//auto start = system_clock::now();
+	//Time Stamp
+	auto dt = 0.01f;
+	// Collection of bodies (particles)
+	Body* universe = new Body[N];
+	// set up simulation
+	startTheBodies(universe);
+	// Iterate and update the forces
+	for (auto i = 0; i < iterations; ++i)
 	{
-		addForces();
-		if (i%20 == 0)
-		{
-			drawImage(bodies, i);
-		}
-		cout << PrintBody(bodies[1]) << endl;
-
+		updateForces(universe, dt);
+		drawImage(universe, i);
 	}
 
-	auto end = system_clock::now();
-	auto total = end - start;
-	cout << "Number of Bodies = " << N << endl;
-	cout << "Main Application time = " << duration_cast<milliseconds>(total).count() << "ms" << endl;
+	//auto end = system_clock::now();
+	//auto total = end - start;
+	//cout << "Number of Bodies = " << N << endl;
+	//cout << "Main Application time = " << duration_cast<milliseconds>(total).count() << "ms" << endl;
 	//drawImage(bodies);
 	//dataFileOutput << duration_cast<milliseconds>(total).count() << endl;
 	return 0;
