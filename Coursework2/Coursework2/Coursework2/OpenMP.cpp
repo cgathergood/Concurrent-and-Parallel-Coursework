4cee2e20d516021e32c7baf6235a6c9e9a2d0bfc
Calum Gathergood <calumgathergood@gmail.com>
2015-12-03 15:58:22 +0000

    Updated OpenMp version

--- Coursework2/Coursework2/Coursework2/OpenMP.cpp
+++ Coursework2/Coursework2/Coursework2/OpenMP.cpp
@@ -6,67 +6,44 @@
 #include <math.h>
 #include <thread>
 #include <omp.h>
+#include <sstream>
+#include "FreeImage/FreeImage.h"
 
 using namespace std;
 using namespace std::chrono;
 
-#define EPS 3e4f // Softening factor
-#define G 6.673e-11f // Gravitational constant
-
-// Output file
-ofstream dataFileOutput("data.csv", ofstream::out);
-
-// Number of bodies
-const int N = 5000;
-
 // Number of threads
 const int num_threads = thread::hardware_concurrency();
 
-const float solarmass = 1.98892e30f;
+// Output file
+ofstream dataFileOutput("data.csv", ofstream::out);
+// Number of bodies
+const int N = 100;
+// Number of iterations
+const int iterations = 100;
 
-// structure of a body (particle)
+// N-Body constants
+const float G = 6.673e-11; // Gravitational Constant
+const float radiusOfUniverse = 1000.0f;
+
+// create and initiliase a new body
 struct Body
 {
-	float rx, ry;//cartesian positions
-	float vx, vy;//velocity components
-	float fx, fy;//force components
-	float mass;//mass
+	double rx, ry;//cartesian positions
+	double vx, vy;//velocity components
+	double fx, fy;//force components
+	double mass;//mass
 };
 
+// Global Bodies
+Body bodies[N];
+
 // convert to string representation formatted nicely
-void PrintBody(Body body)
+string PrintBody(Body body)
 {
-	printf("rx: %f, ry: %f, vx: %f, vy:%f, mass:%f \n", body.rx, body.ry, body.vx, body.vy, body.mass);
-}
-
-// compute the net force acting between the b a and b, and add to the net force acting on a
-void bodyForce(Body* bodies, float dt)
-{
-#pragma omp parallel for num_threads(num_threads), shared(bodies), schedule(dynamic)
-	for (int i = 0; i < N; ++i)
-	{
-		// set force to 0
-		auto fx = 0.0f;
-		auto fy = 0.0f;
-		for (int j = 0; j < N; ++j)
-		{
-			if (i != j)
-			{
-				auto dx = bodies[i].rx - bodies[j].rx;
-				auto dy = bodies[i].ry - bodies[j].ry;
-				auto distance = sqrt(dx * dx + dy * dy);
-
-				auto F = (G * bodies[j].mass * bodies[i].mass) / (distance * distance + EPS * EPS);
-				fx += F * dx / distance;
-				fy += F * dy / distance;
-			}
-		}
-
-		bodies[i].vx += dt * fx / bodies[i].mass;
-		bodies[i].vy += dt * fy / bodies[i].mass;
-		bodies[i].rx += dt * bodies[i].vx;
-		bodies[i].ry += dt * bodies[i].vy;
-	}
+	stringstream ss;
+	ss << "rx:" << body.rx << ", ry:" << body.ry << ", vx:" << body.vx << ", vy:" << body.vy << ", mass:" << body.mass << ", fx:" << body.fx << ", fy:" << body.fy << "\n";
+	return ss.str();
 }
 
 // Random number generator
@@ -75,78 +52,108 @@ float randomGenerator(float min, float max)
 	return ((float(rand()) / float(RAND_MAX)) * (max - min)) + min;
 }
 
-// bodies are initliased in circular orbits around the central mass. This is the physics to do that
-float circlev(float rx, float ry)
-{
-	auto r2 = sqrt(rx * rx + ry * ry);
-	float numerator = (6.67e-11) * 1e6 * solarmass;
-	return sqrt(numerator / r2);
-}
-
-// template for the signum function contained within Java's Math library
-template <typename T>
-int signum(T val)
-{
-	return (T(0) < val) - (val < T(0));
-}
-
 // Initialise N bodies with random positions and circular velocities
 void startTheBodies(Body* bodies)
 {
-	bodies[0].rx = 0.0f;
-	bodies[0].ry = 0.0f;
-	bodies[0].vx = 0.0f;
-	bodies[0].vy = 0.0f;
-	bodies[0].mass = solarmass;
-
 	for (auto i = 0; i < N; ++i)
 	{
-		float px = 1e18 * exp(-1.8) * (.5 - randomGenerator(0.0, 1.0));
-		float py = 1e18 * exp(-1.8) * (.5 - randomGenerator(0.0, 1.0));
-		auto magv = circlev(px, py);
-
-		auto absangle = atan(abs(py / px));
-		float thetav = M_PI / 2 - absangle;
-		auto vx = -1 * signum(py) * cos(thetav) * magv;
-		auto vy = signum(px) * sin(thetav) * magv;
-
-		// Orientate a random 2D cirular orbit
-		if (randomGenerator(0.0, 1.0) <= 0.5)
-		{
-			vx = -vx;
-			vy = -vy;
-		}
-
-		// Calculate mass
-		float mass = solarmass * randomGenerator(0.0, 1.0) * 10 + 1e20;
-		// Assign variables to a body struct
+		auto px = randomGenerator(0, radiusOfUniverse);
+		auto py = randomGenerator(0, radiusOfUniverse);
+		auto m = randomGenerator(0, 10e4);
 		bodies[i].rx = px;
 		bodies[i].ry = py;
-		bodies[i].vx = vx;
-		bodies[i].vy = vy;
-		bodies[i].mass = mass;
+		bodies[i].vx = 0.0f;
+		bodies[i].vy = 0.0f;
+		bodies[i].mass = m;
 	}
-};
+}
+
+void updateForces(Body* bodies, float dt)
+{
+#pragma omp parallel for num_threads(num_threads), shared(bodies), schedule(dynamic)
+	for (auto i = 0; i < N; ++i)
+	{
+		// Reset Forces
+		auto fx = 0.0f;
+		auto fy = 0.0f;
+
+		for (auto j = 0; j < N; ++j)
+		{
+			if (i != j)
+			{
+				// Calcululate new forces
+				float dx = bodies[j].rx - bodies[i].rx;
+				float dy = bodies[j].ry - bodies[i].ry;
+				auto distsqr = dx * dx + dy * dy + 1e-9f; //softening
+				auto distSixth = distsqr * distsqr;
+				auto invDist = 1.0f / sqrtf(distSixth);
+				float F = bodies[j].mass * invDist;
+				fx += F * dx;
+				fy += F * dy;
+			}
+		}
+		// Calculate new velcocity (based on new force)
+		bodies[i].vx += dt * fx;
+		bodies[i].vy += dt * fy;
+	}
+	// Calculate new position (based on new velocity)
+	for (auto i = 0; i < N; ++i)
+	{
+		bodies[i].rx += bodies[i].vx * dt;
+		bodies[i].ry += bodies[i].vy * dt;
+	}
+}
+
+void drawImage(Body bodies[N], int name)
+{
+	FreeImage_Initialise();
+	auto bitmap = FreeImage_Allocate(radiusOfUniverse, radiusOfUniverse, 24);
+	RGBQUAD color;
+
+	for (auto i = 0; i < N; i++)
+	{
+		color.rgbGreen = 255;
+		color.rgbBlue = 255;
+		color.rgbRed = 255;
+		FreeImage_SetPixelColor(bitmap, bodies[i].rx, bodies[i].ry, &color);
+	}
+
+	// Creates a numbered file name
+	stringstream fileName;
+	fileName << name << "test.png";
+	char file[1024];
+	strcpy(file, fileName.str().c_str());
+
+	// Save the file
+	if (FreeImage_Save(FIF_PNG, bitmap, file, 0))
+	{
+		cout << "Image saved - " << fileName.str() << endl;
+	}
+
+	FreeImage_DeInitialise();
+}
 
 int main()
 {
+	//auto start = system_clock::now();
+	//Time Stamp
 	auto dt = 0.01f;
-	Body *bodies = new Body[N];
-
-	auto start = system_clock::now();
-	startTheBodies(bodies);
-	bodyForce(bodies, dt);
-	auto end = system_clock::now();
-	auto total = end - start;
-
-	for (int i = 0; i < N; ++i)
+	// Collection of bodies (particles)
+	Body* universe = new Body[N];
+	// set up simulation
+	startTheBodies(universe);
+	// Iterate and update the forces
+	for (auto i = 0; i < iterations; ++i)
 	{
-		PrintBody(bodies[i]);
+		updateForces(universe, dt);
+		drawImage(universe, i);
 	}
 
-	cout << "Number of Bodies = " << N << endl;
-	cout << "Main Application time = " << duration_cast<milliseconds>(total).count() << "ms" << endl;
-	dataFileOutput << duration_cast<milliseconds>(total).count() << endl;
-
+	//auto end = system_clock::now();
+	//auto total = end - start;
+	//cout << "Number of Bodies = " << N << endl;
+	//cout << "Main Application time = " << duration_cast<milliseconds>(total).count() << "ms" << endl;
+	//drawImage(bodies);
+	//dataFileOutput << duration_cast<milliseconds>(total).count() << endl;
 	return 0;
 }
